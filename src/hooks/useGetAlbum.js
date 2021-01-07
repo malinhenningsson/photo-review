30be4d45ddd4c7dfccfc7f4b620f4b14aa2b0ad3
Malin Henningsson <malinhenningsson@live.se>
2021-01-07 15:24:34 +0100

    Albums visible only to its owner

--- src/hooks/useGetAlbum.js
+++ src/hooks/useGetAlbum.js
@@ -1,10 +1,12 @@
 import {useEffect, useState} from 'react'
 import { db } from '../firebase'
+import { useAuthContext } from '../contexts/AuthContext'
 
 const useGetAlbum = (albumId) => {
     const [album, setAlbum] = useState();
 	const [photos, setPhotos] = useState([]);
     const [loading, setLoading] = useState(true);
+    const { authUser } = useAuthContext();
 
 	useEffect(() => {
 		db.collection('albums').doc(albumId).get().then(doc => {
@@ -17,7 +19,8 @@ const useGetAlbum = (albumId) => {
 
 	useEffect(() => {
 		const unsubscribe = db.collection('images')
-			.where('album', '==', db.collection('albums').doc(albumId))
+            .where('album', '==', db.collection('albums').doc(albumId))
+            .where('owner', '==', authUser.uid)
 			.orderBy("name")
 			.onSnapshot(snapshot => {
 				setLoading(true);
--- src/hooks/useGetAlbums.js
+++ src/hooks/useGetAlbums.js
@@ -1,22 +1,27 @@
 import { useEffect, useState } from 'react'
 import { db } from '../firebase'
+import { useAuthContext } from '../contexts/AuthContext';
 
 const useGetAlbums = () => {
     const [albums, setAlbums] = useState(null);
     const [loading, setLoading] = useState(true);
+    const { authUser } = useAuthContext();
 
     useEffect(() => {
-        const unsubscribe = db.collection('albums').orderBy('title').onSnapshot(snapshot => {
-            setLoading(true);
-            const snapshotAlbums = [];
-            snapshot.forEach(doc => {
-                snapshotAlbums.push({
-                    id: doc.id,
-                    ...doc.data()
+        const unsubscribe = db.collection('albums')
+            .where('owner', '==', authUser.uid)
+            .orderBy('title')
+            .onSnapshot(snapshot => {
+                setLoading(true);
+                const snapshotAlbums = [];
+                snapshot.forEach(doc => {
+                    snapshotAlbums.push({
+                        id: doc.id,
+                        ...doc.data()
+                    })
                 })
-            })
-            setAlbums(snapshotAlbums);
-            setLoading(false);
+                setAlbums(snapshotAlbums);
+                setLoading(false);
         })
         return unsubscribe;
     }, [])
