ce4471ffd79bab673a2ba0e83d4798b354d9b07f
Malin Henningsson <malinhenningsson@live.se>
2021-01-13 16:53:26 +0100

    Fix auth bug while reviewing photos

--- src/components/albums/PhotoGrid.js
+++ src/components/albums/PhotoGrid.js
@@ -8,64 +8,64 @@ const PhotoGrid = ({ photo, albumId, handleDeletePhoto, updateSelectedPhotos, up
     const { authUser } = useAuthContext();
 
     return (
-        <Col xs={12} sm={6} md={4} lg={3} key={photo.id}>
-            <Card className="mb-4">
-                <a href={photo.url} >
-                    <Card.Img variant="top" src={photo.url} />
-                </a>
-                <Card.Body className="d-flex justify-content-between" id={photo.id}>
-                    {
-                        authUser ? (
-                            <>
-                                <div>
-                                    <input 
-                                        type="checkbox" 
-                                        id={photo.id} 
-                                        name="selected-photo" 
-                                        className="mr-2" 
-                                        onChange={updateSelectedPhotos}
-                                        />
-                                    <label htmlFor="selected-photo" className="ms-2">Select</label>
-                                </div>
-                                <div>
+            <Col xs={12} sm={6} md={4} lg={3} key={photo.id}>
+                <Card className="mb-4">
+                    <a href={photo.url} >
+                        <Card.Img variant="top" src={photo.url} />
+                    </a>
+                    <Card.Body className="d-flex justify-content-between" id={photo.id}>
+                        {
+                            authUser.uid === photo.owner ? (
+                                <>
+                                    <div>
+                                        <input 
+                                            type="checkbox" 
+                                            id={photo.id} 
+                                            name="selected-photo" 
+                                            className="mr-2" 
+                                            onChange={updateSelectedPhotos}
+                                            />
+                                        <label htmlFor="selected-photo" className="ms-2">Select</label>
+                                    </div>
+                                    <div>
+                                        <button 
+                                            style={{ border: "none", backgroundColor: "transparent" }} 
+                                            onClick={() => handleDeletePhoto(photo, albumId)}>
+                                                <FontAwesomeIcon 
+                                                    icon={faTrashAlt}
+                                                    style={{ color: "#fff"}}
+                                                />
+                                        </button>
+                                    </div>
+                                </>
+                            ) : (
+                                <>
                                     <button 
                                         style={{ border: "none", backgroundColor: "transparent" }} 
-                                        onClick={() => handleDeletePhoto(photo, albumId)}>
+                                        className="thumbs-up"
+                                        onClick={() => updatePhotoReaction(photo, true)} >
                                             <FontAwesomeIcon 
-                                                icon={faTrashAlt}
-                                                style={{ color: "#fff"}}
-                                            />
+                                                icon={faThumbsUp}
+                                                style={{ fontSize: "1.5em", margin: "0 0.5em" }} 
+                                                />
                                     </button>
-                                </div>
-                            </>
-                        ) : (
-                            <>
-                                <button 
-                                    style={{ border: "none", backgroundColor: "transparent" }} 
-                                    className="thumbs-up"
-                                    onClick={() => updatePhotoReaction(photo, true)} >
-                                        <FontAwesomeIcon 
-                                            icon={faThumbsUp}
-                                            style={{ fontSize: "1.5em", margin: "0 0.5em" }} 
-                                            />
-                                </button>
 
-                                <button 
-                                    style={{ border: "none", backgroundColor: "transparent" }} 
-                                    className="thumbs-down"
-                                    onClick={() => updatePhotoReaction(photo, false)} >
-                                        <FontAwesomeIcon 
-                                            icon={faThumbsDown} 
-                                            style={{ fontSize: "1.5em", margin: "0 0.5em"}} 
-                                            />
-                                </button>
-                            </>
-                        )
-                    }
+                                    <button 
+                                        style={{ border: "none", backgroundColor: "transparent" }} 
+                                        className="thumbs-down"
+                                        onClick={() => updatePhotoReaction(photo, false)} >
+                                            <FontAwesomeIcon 
+                                                icon={faThumbsDown} 
+                                                style={{ fontSize: "1.5em", margin: "0 0.5em"}} 
+                                                />
+                                    </button>
+                                </>
+                            )
+                        }
 
-                </Card.Body>
-            </Card>
-        </Col>
+                    </Card.Body>
+                </Card>
+            </Col>
     )
 }
 
